caf807c60ac3a9361ad8c630c13cabf1803feee3
v1trovao <viitrovao@gmail.com>
2025-04-17 15:18:41 -0300

    Revert "Revisao inicial - docs e pastas"
    This reverts commit 2eebcffe18684e2f0af4fd65b3b8066040ca0088.

--- src/main/java/BiblioApplication.java
+++
@@ -1,8 +0,0 @@
-import view.MenuInicial;
-
-public class BiblioApplication {
-    public static void main(String[] args) {
-        MenuInicial menu = new MenuInicial();
-        menu.executar();
-    }
-}
