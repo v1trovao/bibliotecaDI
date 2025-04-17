3f7fcc21a88edb7468b7f7af5960463b484d4737
v1trovao <viitrovao@gmail.com>
2025-04-17 15:25:48 -0300

    Revert "Revert "Revisao inicial - docs e pastas""
    This reverts commit caf807c60ac3a9361ad8c630c13cabf1803feee3.

---
+++ src/main/java/view/MenuLivros.java
@@ -0,0 +1,58 @@
+package view;
+
+import javax.swing.*;
+
+public class MenuLivros {
+
+    public void exibir() {
+        int opcao;
+        do {
+            opcao = Integer.parseInt(JOptionPane.showInputDialog("\t << Livros >> \n " +
+                    "Escolha a operação: \n" +
+                    "[1] Cadastrar Livro \n" +
+                    "[2] Atualizar Livro \n" +
+                    "[3] Remover Livro \n" +
+                    "[4] Listar Livros \n" +
+                    "[5] Emprestar Livro \n" +
+                    "[6] Devolver Livro \n" +
+                    "[7] Reservar Livro \n" +
+                    "[8] Cancelar Reserva \n" +
+                    "[9] Voltar \n"));
+
+            switch (opcao) {
+                case 1:
+                    String titulo = JOptionPane.showInputDialog("Digite o título do livro:");
+                    String nomeAutor = JOptionPane.showInputDialog("Digite o nome do autor:");
+                    String nomeEditora = JOptionPane.showInputDialog("Digite o nome da editora:");
+                    // OperacoesLivros.cadastrarLivro(catalogoLivros, catalogoAutores, catalogoEditoras);
+                    break;
+                case 2:
+                    // OperacoesLivros.atualizarLivro(catalogoLivros, catalogoAutores, catalogoEditoras);
+                    break;
+                case 3:
+                    // OperacoesLivros.removerLivro(catalogoLivros);
+                    break;
+                case 4:
+                    // OperacoesLivros.listarLivros(catalogoLivros);
+                    break;
+                case 5:
+                    // OperacoesLivros.emprestarLivro(catalogoLivros);
+                    break;
+                case 6:
+                    // OperacoesLivros.devolverLivro(catalogoLivros);
+                    break;
+                case 7:
+                    // OperacoesLivros.reservarLivro(catalogoLivros);
+                    break;
+                case 8:
+                    // OperacoesLivros.cancelarReserva(catalogoLivros);
+                    break;
+                case 9:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida.");
+                    break;
+            }
+        } while (opcao != 9);
+    }
+}
